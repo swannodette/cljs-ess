2745d48ca366706e7017ae3ee2052852a35e771c
David Nolen <david.nolen@gmail.com>
2018-07-08 19:54:08 -0400

    typos

--- src/clj/cljs_ess/server.clj
+++ src/clj/cljs_ess/server.clj
@@ -3,17 +3,17 @@
             [io.pedestal.http.route :as route]))
 
 (defn respond-hello [request]          
-  {:status 200 :body "Hello, world!"}
+  {:status 200 :body "Hello, world!"})
 
 (def routes
   (route/expand-routes                                   
-    #{["/" :get respond-hello :route-name :greet]}
+    #{["/" :get respond-hello :route-name :greet]}))
 
 (defn create-server []
   (http/create-server     
     {::http/routes routes  
      ::http/type   :jetty  
-     ::http/port   8890 
+     ::http/port   8890}))
       
 (defn start []
-  (http/start (create-server))))
+  (http/start (create-server)))
