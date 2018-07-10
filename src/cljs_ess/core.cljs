bb2d9487e11b34c0431b8a457f33b8e0753f4305
dnolen <dnolen@cognitect.com>
2018-07-10 17:43:49 -0400

    Clean up first example

--- src/cljs_ess/core.cljs
+++ src/cljs_ess/core.cljs
@@ -2,4 +2,13 @@
   (:require [goog.dom :as gdom]
             [reagent.core :as r]))
 
-(r/render [:p "Hello world!"] (gdom/getElement "app"))
+(def state (r/atom 0))
+
+(defn counter [text]
+  [:div
+    [:p (str "Hello Reagent! I've counted " @state " clicks so far")]
+    [:button 
+      {:on-click (fn [e] (swap! state inc))} 
+      "Click Me!"]])
+
+(r/render [counter state] (gdom/getElement "app"))
