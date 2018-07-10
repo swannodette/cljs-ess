ede6713765098d54b3a9ecbd2bc853cc259b960b
dnolen <dnolen@cognitect.com>
2018-07-10 18:10:02 -0400

    add project.clj, update .gitignore

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cljs-ess "0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.339"]
+                 [reagent/reagent "0.8.1"]
+                 [com.bhauman/figwheel-main "0.1.4"]
+                 [com.bhauman/rebel-readline-cljs "0.1.4"]]) 
