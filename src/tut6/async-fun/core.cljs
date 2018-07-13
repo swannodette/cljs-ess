d9d791f40b64352f408c37e2abf62f28dc87db15
dnolen <dnolen@cognitect.com>
2018-07-12 22:28:40 -0400

    core.async basic

--- src/tut6/async-fun/core.cljs
+++ src/tut6/async-fun/core.cljs
@@ -1,10 +1,21 @@
 (ns async-fun.core
   (:require [cljs.core.async :refer [put! take! chan go >! <!]]))
 
-(defn start-process [c]
+(def my-chan (chan))
+
+(defn start-process1 [c]
   (go
     (println "Got a value:" (<! c))
     (println "Got another value:" (<! c))
     (println "And another value:" (<! c))
     (println "Enough is enough!")))
 
+(defn start-process2 [c]
+  (go
+    (println "Have a number:")
+    (>! c 1)
+    (println "Have a string:")
+    (>! c "cool!")
+    (println "Have a vector:")
+    (>! c [:foo :bar :baz])
+    (println "I'm all out of values!")))
