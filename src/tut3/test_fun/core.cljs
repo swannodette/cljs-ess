3c509b12f674c76014c0042259015204c40558ed
dnolen <dnolen@cognitect.com>
2018-07-12 20:16:13 -0400

    reorg again

---
+++ src/tut3/test_fun/core.cljs
@@ -0,0 +1,9 @@
+(ns test-fun.core
+  (:require [cljs.test :as test :refer [deftest is run-tests]]))
+
+(deftest test-add
+  (is (= (+ 1 1) 2)))
+
+(deftest test-conj
+  (is (= (conj [1 2] 3)
+         [1 2 3])))
