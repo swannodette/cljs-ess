4e08b8190e53785f48f0e3074ebd94e828e1da4a
dnolen <dnolen@cognitect.com>
2018-07-12 22:18:20 -0400

    wip

--- src/tut6/async-fun/core.cljs
+++ src/tut6/async-fun/core.cljs
@@ -1,1 +1,10 @@
-(ns async-fun.core)
+(ns async-fun.core
+  (:require [cljs.core.async :refer [put! take! chan go >! <!]]))
+
+(defn start-process [c]
+  (go
+    (println "Got a value:" (<! c))
+    (println "Got another value:" (<! c))
+    (println "And another value:" (<! c))
+    (println "Enough is enough!")))
+
